4aaec593d7dac15ba2f8c8d1d4b56f43b186e0a7
Dipakkamble <kambledipak700@gmail.com>
2023-01-21 00:00:27 +0530

    [Dipak] Modified Calculating EmployeeAttendance

--- EmployeeWageComputation/Program.cs
+++ EmployeeWageComputation/Program.cs
@@ -8,9 +8,23 @@ namespace EmployeeWageComputation
 {
     internal class Program
     {
+        
         static void Main(string[] args)
         {
+            int isPresent = 1;
             Console.WriteLine("Welcome to employee wage computation");
+
+            //UC1-EmployeeAttendance
+            Random random = new Random();
+            int empAttendance=random.Next(0,2); // 0 or 1 it will generate
+            if ( empAttendance == isPresent )
+            {
+                Console.WriteLine("Employee is present");
+            }
+            else 
+            {
+                Console.WriteLine("Employee is absent");
+            }
             Console.ReadLine();
         }
     }
